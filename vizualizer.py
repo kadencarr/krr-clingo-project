af42321c11b04245128d8e29890fa81830e94437
Daniel Hill <dhhillaz@gmail.com>
2019-09-15 09:59:58 -0700

    starting init

--- vizualizer.py
+++ vizualizer.py
@@ -1,5 +1,52 @@
 import re
 
+init = """
+init(object(node,1),value(at,pair(1,1))).
+init(object(node,2),value(at,pair(2,1))).
+init(object(node,3),value(at,pair(3,1))).
+init(object(node,4),value(at,pair(4,1))).
+init(object(node,5),value(at,pair(1,2))).
+init(object(node,6),value(at,pair(2,2))).
+init(object(node,7),value(at,pair(3,2))).
+init(object(node,8),value(at,pair(4,2))).
+init(object(node,9),value(at,pair(1,3))).
+init(object(node,10),value(at,pair(2,3))).
+init(object(node,11),value(at,pair(3,3))).
+init(object(node,12),value(at,pair(4,3))).
+init(object(node,13),value(at,pair(1,4))).
+init(object(node,14),value(at,pair(2,4))).
+init(object(node,15),value(at,pair(3,4))).
+init(object(node,16),value(at,pair(4,4))).
+init(object(highway,4),value(at,pair(4,1))).
+init(object(highway,8),value(at,pair(4,2))).
+init(object(highway,12),value(at,pair(4,3))).
+init(object(highway,13),value(at,pair(1,4))).
+init(object(highway,14),value(at,pair(2,4))).
+init(object(highway,15),value(at,pair(3,4))).
+init(object(highway,16),value(at,pair(4,4))).
+init(object(pickingStation,1),value(at,pair(1,3))).
+init(object(pickingStation,2),value(at,pair(3,1))).
+init(object(robot,1),value(at,pair(4,3))).
+init(object(robot,2),value(at,pair(2,2))).
+init(object(shelf,1),value(at,pair(3,3))).
+init(object(shelf,2),value(at,pair(2,1))).
+init(object(shelf,3),value(at,pair(2,3))).
+init(object(shelf,4),value(at,pair(2,2))).
+init(object(shelf,5),value(at,pair(3,2))).
+init(object(shelf,6),value(at,pair(1,2))).
+init(object(product,1),value(on,pair(3,1))).
+init(object(product,2),value(on,pair(4,1))).
+init(object(product,3),value(on,pair(6,4))).
+init(object(product,4),value(on,pair(5,1))).
+init(object(product,4),value(on,pair(6,1))).
+init(object(order,1),value(pickingStation,1)).
+init(object(order,1),value(line,pair(1,1))).
+init(object(order,1),value(line,pair(3,4))).
+init(object(order,2),value(pickingStation,2)).
+init(object(order,2),value(line,pair(2,1))).
+init(object(order,3),value(pickingStation,2)).
+init(object(order,3),value(line,pair(4,1))).
+"""
 
 output = """
 occurs(object(robot,1),move(-1,0),1) 
@@ -28,6 +75,7 @@ occurs(object(robot,2),move(1,0),12)
 occurs(object(robot,1),deliver(2,2,1),13)
 """
 
+# Parse
 action_list = output.split()
 organized_actions = {}
 for action_string in action_list:
@@ -36,5 +84,12 @@ for action_string in action_list:
     robot_id = re.search('\d*(?=\)\,)',action_string).group(0)
     action = re.search('(?<=\)\,)\w\w',action_string).group(0)
     action_params = [x for x in re.findall('-?\d*',action_string) if x != ''][1:-1]
-    print(action_string)
-    print(robot_id, timestep, action, action_params)
+    if timestep not in organized_actions:
+        organized_actions[timestep] = []
+    organized_actions[timestep].append({
+        'robot_id':robot_id,
+        'action':action,
+        'action_params':action_params
+    })
+
+# Visualize
